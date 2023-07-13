3132ec8005a83e542bfd8a50ca6ac0b70492f444
大川原誠人 <ookawaramasato@ookawaramasatonoMacBook-Pro.local>
2023-07-14 01:34:21 +0900

    第3回課題提出（修正版1）

---
+++ src/Exception_Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exception_Main {
+    public static void main(String[] age){
+
+        List<String> names = new ArrayList<>();
+        names.add("ノーゲーム・ノーライフ1");
+        names.add("ノーゲーム・ノーライフ2");
+        names.add("ライアー・ライアー1");
+        names.add("ライアー・ライアー2");
+        
+        System.out.println(names.get(4));
+    }
+}
